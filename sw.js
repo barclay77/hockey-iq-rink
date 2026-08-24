d23924148f2d83226a457880c6f6599ad8471800
authln-mike <mike@authln.com>
2026-08-24 13:19:08 -0400

    Deploy build 2026-08-24.153
    - index.html
    - sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Above The Puck - offline cache.
    Bump CACHE when you change any file, otherwise browsers keep serving the old one. */
-const CACHE = 'hockey-iq-v155';
+const CACHE = 'hockey-iq-v156';
 const ASSETS = [
   './',
   './index.html',
